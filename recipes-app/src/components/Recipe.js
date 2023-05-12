d6f5d88bf1f556d60d1776c735df5e771f8767cc
haka <alexsveshnik@gmail.com>
2023-05-12 23:01:22 +0300

    пытался установить tailwind, не вышло пока

--- recipes-app/src/components/Recipe.js
+++ recipes-app/src/components/Recipe.js
@@ -4,7 +4,7 @@ import Instructions from "./Instructions";
 
 function Recipe({ name, ingredients, steps }) {
     return (
-        <section id={name.toLowerCase().replace(/ /g, "-")}>
+        <section id={name.toLowerCase().replace(/ /g, "-")} className="rounded-2xl border border-gray-200 p-8">
             <h1>{name}</h1>
             <IngredientsList list={ingredients} />
             <Instructions title="Cooking Instructions" steps={steps} />
