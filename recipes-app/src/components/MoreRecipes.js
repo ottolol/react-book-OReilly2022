0a8653863ad480e192ace1f19eceaa42a908b23c
haka <alexsveshnik@gmail.com>
2023-05-15 21:19:50 +0300

    подправил More Recipes, доабвил новое фото и стили

--- recipes-app/src/components/MoreRecipes.js
+++ recipes-app/src/components/MoreRecipes.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function MoreRecipes () {
+function MoreRecipes() {
     return (
         <div className="moreRecipes">
-            <section className="flex flex-col overflow-hidden rounded-3xl p-6 mb-4 shadow-lg shadow-gray-900/5 bg-white">
-                <h3 className="mb-4 text-4xl font-medium tracking-tight text-gray-900">More Recipes</h3>
-                <div className="font-semibold text-gray-900 mb-4">Mini pizza</div>
-                <div><img src="recipes1.png" alt="" className="w-96 rounded-3xl" /></div>
+            <h3 className="mb-4 text-4xl font-medium tracking-tight text-gray-900">More Recipes</h3>
+            <section className="flex flex-row overflow-hidden rounded-3xl p-6 mb-4 shadow-lg shadow-gray-900/5 bg-white">
+                <div className="me-[15px]">
+                    <div className="font-semibold text-gray-900 mb-4">Mini pizza</div>
+                    <div><img src="recipes1.png" alt="" className="w-80 rounded-3xl" /></div>
+                </div>
+                <div className="">
+                    <div className="font-semibold text-gray-900 mb-4">Mac with chicken fillet</div>
+                    <div><img src="recipes2.png" alt="" className="w-80 rounded-3xl" /></div>
+                </div>
             </section>
         </div>
     );
