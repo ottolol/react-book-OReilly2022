b684bd0b1798db76479bd7777abece78add18977
haka <alexsveshnik@gmail.com>
2023-05-17 22:08:33 +0300

    прокинули пропсы: style, onDoubleClick из Recipe.js в StarRating.js

--- recipes-app/src/components/StarRating.js
+++ recipes-app/src/components/StarRating.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({
-    totalStars = 5,
-    selectedStars = 0,
-    onRate = f => f
-}) {
+// export default function StarRating({ totalStars = 5 }) {
+//     return createArray(totalStars).map((n, i) => <Star key={i} />);
+// }
+
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
+    const [selectedStars, setSelectedStars] = useState(0);
     return (
-        <>
+        <div className="flex pt-2" style={{ padding: "", ...style }} {...props}>
             {createArray(totalStars).map((n, i) => (
                 <Star
                     key={i}
                     selected={selectedStars > i}
-                    onSelect={() => onRate(i + 1)}
+                    onSelect={() => setSelectedStars(i + 1)}
                 />
             ))}
-        </>
+            <p className="pl-2">
+                {selectedStars} of {totalStars} stars
+            </p>
+        </div>
     );
 }
 
