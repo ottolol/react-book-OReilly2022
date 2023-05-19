6016c1e1dfa53b0e5018ebe8ace40ff290b22301
haka <alexsveshnik@gmail.com>
2023-05-19 22:53:42 +0300

    добавил состояние onRemoveColor, чтобы можно было удалять цвета

--- recipes-app/src/components/ColorOrganizer.js
+++ recipes-app/src/components/ColorOrganizer.js
@@ -1,8 +1,23 @@
+// ColorOrganizer - это родительский компонент, компонента ColorList, в котром хранится состояние 
+
 import React, { useState } from "react";
 import colorData from "../data/color-data.json";
 import ColorList from "./ColorList.js";
 
 export default function App() {
-    const [colors] = useState(colorData);
-    return <ColorList colors={colors} />;
+    const [colors, setColors] = useState(colorData);
+    return (
+        <ColorList
+            colors={colors}
+            onRemoveColor={id => {
+                const newColors = colors.filter(color => color.id !== id);
+                setColors(newColors);
+            }}
+        />
+    );
 }
+
+// export default function App() {
+//     const [colors] = useState(colorData);
+//     return <ColorList colors={colors} />;
+// }
